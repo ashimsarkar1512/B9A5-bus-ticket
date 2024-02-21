aa456a3e6bdb6dacd6ac312823bd7ca4b04abd0b
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2024-02-21 11:58:39 +0600

    finished the all function

--- script/utiliti.js
+++ script/utiliti.js
@@ -25,11 +25,6 @@ document.getElementById('Phone-Num').addEventListener('keyup',function(){
   document.getElementById('modal').removeAttribute('disabled')
 })
 
-document.getElementById("apply-btn").addEventListener("click", function(){
-  
-  document.getElementById("coupon-field").add('hidden');
-  
-});
 
 
 
